5a9b5ff0a293f28a6faa6fdad9d2ea264718e70f
10937618 <10937618@uvu.edu>
2023-03-06 22:46:47 -0700

    more php

---
+++ drawtwobody.php
@@ -0,0 +1,16 @@
+<?php 
+session_start();
+require 'autoload.php';
+foreach($card_array as $card) : ?>
+       <img src="<?php echo $card['image'];?>">
+   <?php endforeach; ?>
+   <h1><?php echo "Your card total is $card_total"; ?></h1>
+   <?php if($card_total > 21): ?>
+       Sorry your total is above 21
+       <a href="index.html">Play Again</a>
+   <?php elseif($card_total == 21): ?>
+       You win, take a trip to Vegas
+       <a href="index.html">Play Again</a>
+   <?php else: ?>
+       <a href="drawagain.html">Draw again</a>
+   <?php endif; ?>
